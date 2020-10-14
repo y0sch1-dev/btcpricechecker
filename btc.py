bbbd9e47eaf11d663254ac37c54aea0ea4e7081a
y0sch1 <39857327+y0sch1-dev@users.noreply.github.com>
2020-10-14 18:49:30 +0200

    Update btc.py

--- btc.py
+++ btc.py
@@ -1,1 +1,15 @@
+import requests
+import json
+import time
 
+lastPrice = 0
+def BtcPrice():
+    print(newPrice,"euro")
+while True:
+    url = requests.get('https://api.binance.com/api/v1/ticker/price?symbol=BTCEUR')
+    data = url.json()
+    newPrice = float(data['price'])
+    if (newPrice != lastPrice):
+        lastPrice = newPrice
+        BtcPrice()
+    time.sleep(15)
